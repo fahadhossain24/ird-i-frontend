90c156b57a16ebb32e02eec1068d28acd927cf8e
fahadhossain24 <fahadhossaim24@gmail.com>
2023-11-08 07:45:30 +0600

    initial commit. setup ird dua project with next

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Dua & Ruqyah | All Duas Collection',
+  description: 'created by Fahad Hossain',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h3 className="text-center bg-red-500">welcome</h3>
+    </main>
+  )
+}
